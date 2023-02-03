c3d4552a30447a6a35300c48d7b644e4765b08f2
Chris Nesbitt-Smith <chris@cns.me.uk>
2023-02-03 23:42:09 +0000

    fix some typos that broke it

--- main.tf
+++ main.tf
@@ -2,24 +2,24 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+resource "azurerm_resource_group" "cnsdemo" {
+  name     = "cnsdemo-resources"
   location = "UK South"
 }
 
 resource "azurerm_service_plan" "cnsdemo" {
   name                = "cnsdemo"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.cnsdemo.name
+  location            = azurerm_resource_group.cnsdemo.location
   os_type             = "Linux"
-  sku_name            = "P1v2"
+  sku_name            = "S1"
 }
 
 resource "azurerm_linux_web_app" "cnsdemo" {
   name                = "cnsdemoapp"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_service_plan.example.location
-  service_plan_id     = azurerm_service_plan.example.id
+  resource_group_name = azurerm_resource_group.cnsdemo.name
+  location            = azurerm_service_plan.cnsdemo.location
+  service_plan_id     = azurerm_service_plan.cnsdemo.id
   site_config {
     application_stack {
       docker_image     = "registry.hub.docker.com/library/nginx"
@@ -39,46 +39,46 @@ resource "azurerm_linux_web_app" "cnsdemo" {
     RESOLVER                              = "8.8.8.8"
     NGINX_ENVSUBST_OUTPUT_DIR             = "/etc/nginx"
     API_ENDPOINT                          = "api.github.com"
-    FORCE_REFRESH_CONFIG                  = "${md5(file("default.conf"))}:${md5(file("nginx.js"))}"
+    FORCE_REFRESH_CONFIG                  = "${md5(file("nginx.conf"))}:${md5(file("nginx.js"))}"
   }
   storage_account {
-    access_key   = azurerm_storage_account.example.primary_access_key
-    account_name = azurerm_storage_account.example.name
+    access_key   = azurerm_storage_account.cnsdemo.primary_access_key
+    account_name = azurerm_storage_account.cnsdemo.name
     name         = "config"
-    share_name   = azurerm_storage_container.example.name
+    share_name   = azurerm_storage_container.cnsdemo.name
     type         = "AzureBlob"
     mount_path   = "/etc/nginx/templates"
   }
 }
 
 
-resource "azurerm_storage_account" "example" {
+resource "azurerm_storage_account" "cnsdemo" {
   name                     = "cnsdemo"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+  resource_group_name      = azurerm_resource_group.cnsdemo.name
+  location                 = azurerm_resource_group.cnsdemo.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-resource "azurerm_storage_container" "example" {
+resource "azurerm_storage_container" "cnsdemo" {
   name                  = "content"
-  storage_account_name  = azurerm_storage_account.example.name
+  storage_account_name  = azurerm_storage_account.cnsdemo.name
   container_access_type = "private"
 }
 
 resource "azurerm_storage_blob" "nginx_config" {
   name                   = "nginx.conf.template"
-  content_md5            = md5(file("default.conf"))
-  storage_account_name   = azurerm_storage_account.example.name
-  storage_container_name = azurerm_storage_container.example.name
+  content_md5            = md5(file("nginx.conf"))
+  storage_account_name   = azurerm_storage_account.cnsdemo.name
+  storage_container_name = azurerm_storage_container.cnsdemo.name
   type                   = "Block"
   source                 = "nginx.conf"
 }
 resource "azurerm_storage_blob" "nginx_js" {
   name                   = "nginx.js"
   content_md5            = md5(file("nginx.js"))
-  storage_account_name   = azurerm_storage_account.example.name
-  storage_container_name = azurerm_storage_container.example.name
+  storage_account_name   = azurerm_storage_account.cnsdemo.name
+  storage_container_name = azurerm_storage_container.cnsdemo.name
   type                   = "Block"
   source                 = "nginx.js"
 }
